6058813a9e3de03fefd32bfa54d93035f42bd596
Ashane Silva <ashanecool@gmail.com>
2025-04-01 16:51:40 +0530

    chore: remove multiple products from product.ts

--- app/data/products.ts
+++ app/data/products.ts
@@ -10,21 +10,21 @@ export const products = [
       "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
     slug: "fall-limited-edition-sneakers",
   },
-  {
-    id: 2,
-    image: image1_thumb,
-    name: "summer vakers",
-    price: 1525.0,
-    description:
-      "These low-profcvle sneakers are your pnnbnerfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
-    slug: "fall-limited-vneditiocbn-sneakers",
-  },
-  {
-    id: 3,
-    image: image1_thumb,
-    name: "show Sneakers",
-    price: 155.0,
-    description:
-      "These low-profile f"
-  },
+  // {
+  //   id: 2,
+  //   image: image1_thumb,
+  //   name: "summer vakers",
+  //   price: 1525.0,
+  //   description:
+  //     "These low-profcvle sneakers are your pnnbnerfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.",
+  //   slug: "fall-limited-vneditiocbn-sneakers",
+  // },
+  // {
+  //   id: 3,
+  //   image: image1_thumb,
+  //   name: "show Sneakers",
+  //   price: 155.0,
+  //   description:
+  //     "These low-profile f"
+  // },
 ];
